bb9b65b11a97d392e129c4b21c5a2315c785be71
horzu <mertsakar@hotmail.com>
2023-11-13 11:11:06 +0300

    Exercise 3.22

---
+++ phonebook-backend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    'rules': {
+        // ...
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ]
+      },
+      'extends': 'eslint:recommended',
+      'no-console': 0   
+}
